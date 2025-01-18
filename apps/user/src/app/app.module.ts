d794b12dfcd6e256d32696bef2ca9cfb97aa6d49
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-01-18 09:55:13 +0300

    Prisma client module

--- apps/user/src/app/app.module.ts
+++ apps/user/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaClientModule } from '@albert-ambaryan/models';
 import { UserModule } from './user.module';
 import { AuthModule } from './auth.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, PrismaClientModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
