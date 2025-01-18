c34ccf3456c6600e72d2f538080dd6b2f4e8accc
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-01-18 15:25:42 +0300

    Add core library

---
+++ libs/shared/core/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/crud-repository.interface';
+export * from './lib/entity.interface';
---
+++ libs/shared/core/src/lib/entity.interface.ts
@@ -0,0 +1,7 @@
+export type EntityIdType = string;
+export type PlainObject = Record<string, unknown>;
+
+export interface Entity<T extends EntityIdType, ObjectType = PlainObject> {
+  id?: T;
+  toPlainObject(): ObjectType;
+}
