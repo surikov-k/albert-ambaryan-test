683b2c724f4c15384af254ce2ff67cc2e60cd224
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-01-19 15:45:11 +0300

    Initial React web app

---
+++ apps/web-app/src/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+
+
+export function App() {
+  return (
+    <div>
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default App;
