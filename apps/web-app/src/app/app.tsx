d0c5c1b1fb8f0732f2633e82d060dca014b89521
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-01-20 13:03:23 +0300

    Forms Animation

--- apps/web-app/src/app/app.tsx
+++ apps/web-app/src/app/app.tsx
@@ -1,11 +1,22 @@
+import { motion } from "framer-motion";
+
 import RegisterForm from "./auth/register-form";
 
 export function App() {
   return (
-    <section className="w-full">
-      <div className="flex h-screen items-center justify-center">
+    <section className="flex min-h-screen w-full items-center justify-center bg-gradient-to-bl from-gray-50 to-gray-100 p-4">
+      <motion.div
+        className="flex w-full justify-center"
+        initial={{ opacity: 0, scale: 0.8, y: -50 }}
+        animate={{ opacity: 1, scale: 1, y: 0 }}
+        transition={{
+          duration: 0.6,
+          delay: 0.3,
+          ease: "easeOut",
+        }}
+      >
         <RegisterForm />
-      </div>
+      </motion.div>
     </section>
   );
 }
